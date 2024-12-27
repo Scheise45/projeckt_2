69f812f69b1f09e92301af18bca24aa1a0eb9180
Switscorn <162159058+Switscorn@users.noreply.github.com>
2024-12-27 08:43:56 +0300

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -1,21 +1,21 @@
 import pygame
 
 s = [
-    [0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 1, 1, 1],
-    [2, 3, 2, 3, 2, 2],
-    [2, 1, 2, 1, 2, 2],
-    [4, 1, 2, 1, 2, 2],
-    [1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [2, 2, 3, 2, 2, 3, 2, 2],
+    [2, 1, 1, 2, 1, 1, 2, 2],
+    [4, 1, 1, 3, 1, 1, 2, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1],
 ]
 pygame.init()
-screen = pygame.display.set_mode((600, 600))
+screen = pygame.display.set_mode((800, 600))
 screen.fill("white")
-hero = pygame.image.load("picture/hero.jpg")
-sky = pygame.image.load("picture/sky.jpg")
-wall = pygame.image.load("picture/wall.jpg")
-lianas = pygame.image.load("picture/lianas.jpg")
-stone = pygame.image.load("picture/stone.jpg")
+hero = pygame.image.load("hero.jpg")
+sky = pygame.image.load("sky.jpg")
+wall = pygame.image.load("wall.jpg")
+lianas = pygame.image.load("lianas.jpg")
+stone = pygame.image.load("stone.jpg")
 fils = [sky, wall, lianas, stone, hero]
 running = True
 while running:
